a9af5c62b2c2d1f4280806208b0393ceedd7f44b
Sourabh Raj <39014867+rajs1006@users.noreply.github.com>
2022-03-21 18:16:19 +0100

    Update FileOperationDS.py

--- FileOperationDS.py
+++ FileOperationDS.py
@@ -1,4 +1,4 @@
-import os
+import yaml
 
 
 class Tree(object):
@@ -34,16 +34,17 @@ class Tree(object):
         if isChildPresent:
             parentNode.child.pop(idx)
 
-    def toJson(self, parent=None, jsonStr={}):
+    def toDict(self, parent=None, jsonStr={}):
 
         parent = parent if parent else self
 
         if parent.child:
             jsonStr[parent.data] = []
+            # Inplace sorting for printing
+            parent.child.sort(key=lambda c: c.data)
             for i, c in enumerate(parent.child):
-
                 jsonStr[parent.data].append({c.data: []} if c.child else c.data)
-                self.toJson(parent=c, jsonStr=jsonStr[parent.data][i])
+                self.toDict(parent=c, jsonStr=jsonStr[parent.data][i])
 
             return jsonStr
 
@@ -72,84 +73,106 @@ class Tree(object):
         return False
 
 
-def makeFileDirectory(location, T):
-    loc = location[0]
-    path = loc.split("/")
-
-    filteredPath = [p for p in path if p]
-    for i in range(len(filteredPath)):
-        if i == 0:
-            T.addChild(filteredPath[i], "/")
-        else:
-            T.addChild(filteredPath[i], filteredPath[i - 1])
-
-    print(T.toJson())
-
-
-def removeFileDirectory(location, T):
-    loc = location[0]
-    path = loc.split("/")
-
-    filteredPath = [p for p in path if p]
-    for i in range(len(filteredPath) - 1, -1, -1):
-        if filteredPath[i].split(".")[-1] != "txt":
+class CommandOperations:
+    @staticmethod
+    def __addChild(T, path):
+        for i in range(len(path) - 1, -1, -1):
             if i == 0:
-                T.removeChild(filteredPath[i], "/")
+                T.addChild(path[i], "/")
             else:
-                T.removeChild(filteredPath[i], filteredPath[i - 1])
-        else:
-            T.removeChild(filteredPath[i], filteredPath[i - 1])
-            break
+                T.addChild(path[i], path[i - 1])
 
-    print(T.toJson())
+    @staticmethod
+    def __removeChild(T, path):
+        for i in range(len(path) - 1, -1, -1):
+            if path[i].split(".")[-1] != "txt":
+                if i == 0:
+                    T.removeChild(path[i], "/")
+                else:
+                    T.removeChild(path[i], path[i - 1])
+            else:
+                T.removeChild(path[i], path[i - 1])
+                break
+
+    @staticmethod
+    def __getCommands(line):
+        command = line.replace("\n", "")
+        cmd = command.split(" ")
+
+        return (cmd[0], cmd[1:])
+
+    def __init__(self) -> None:
+
+        self.__command_dictionary = {
+            "md": self.makeFileDirectory,
+            "mf": self.makeFileDirectory,
+            "mv": self.moveFileDirectory,
+            "rm": self.removeFileDirectory,
+            "cp": self.copyFileDirectory,
+        }
+        self.T = Tree("/")
+
+    def __call__(self, commandLine):
+
+        cmd, path = self.__getCommands(commandLine)
+
+        funct = self.__command_dictionary[cmd]
+        funct(path, self.T)
+
+    def makeFileDirectory(self, location, T):
+        loc = location[0]
+        path = loc.split("/")
+
+        newPath = [p for p in path if p]
+        self.__addChild(T, newPath)
+
+    def removeFileDirectory(self, location, T):
+        loc = location[0]
+        path = loc.split("/")
+
+        filteredPath = [p for p in path if p]
+        self.__removeChild(T, filteredPath)
+
+    def copyFileDirectory(self, location, T):
+
+        newPath = [p for p in location[1].split("/") if p]
+        self.__addChild(T, newPath)
+
+    def moveFileDirectory(self, location, T):
+        originalPath = [p for p in location[0].split("/") if p]
+        newPath = [p for p in location[1].split("/") if p]
+
+        self.__removeChild(T, originalPath)
+        self.__addChild(T, newPath)
+
+    def json(self):
+        return self.T.toDict()
+
+    def __str__(self) -> str:
+        return yaml.dump(self.json(), default_flow_style=False)
 
 
-def copyFileDirectory(location, T):
-
-    originalPath = [p for p in location[0].split("/") if p]
-    newPath = [p for p in location[1].split("/") if p]
-
-    # NOTE : Not worning on this further
-    # newPath.append(originalPath[-1])
-    # for i in range(0, len(newPath) - 2):
-    #     if i == 0:
-    #         T.addChild(newPath[i], "/")
-    #     else:
-    #         T.addChild(newPath[i], newPath[i - 1])
-
-
-def moveFileDirectory(location, T):
-    copyFileDirectory(location, T)
-    removeFileDirectory(location[0], T)
-
-
-_command_dictionary = {
-    "md": makeFileDirectory,
-    "mf": makeFileDirectory,
-    "mv": moveFileDirectory,
-    "rm": removeFileDirectory,
-    "cp": copyFileDirectory,
-}
-
-a = """md /Test
+commands = """md /Test
 md /Dir1
 md /Dir1/Dir2
 md /Dir1/Dir2/NewDir
 mf /Dir1/Dir2/file.txt
 rm /Dir1/Dir2/file.txt
-rm /Dir1/Dir2
+cp /Dir1/Dir2/file.txt /Dir1/Dir2/newfile.txt
+mv /Dir1/Dir2/newfile.txt /Dir1/Dir2/newfile1.txt
+rm /Dir1/Dir4/newfile.txt
+md /Dir3/Dir4/NewDir
 """
 
 if __name__ == "__main__":
 
-    # file = open("a", "r")
-    # lines = file.readlines()
-    T = Tree("/")
+    cmdProcessor = CommandOperations()
 
-    for line in a.splitlines():
-        line = line.replace("\n", "")
-        command = line.split(" ")
-        funct = _command_dictionary[command[0]]
+    for line in commands.splitlines():
+        try:
+            cmdProcessor(line)
+        except Exception as e:
+            print(f"'{line}' failed with error {e}")
 
-        print(f"running funct {funct.__name__}")
-        funct(command[1:], T)
+    print("\n----------- File Structure -------------\n")
+    print(str(cmdProcessor))
